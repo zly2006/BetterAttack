677cd08acebe8e9006e4f3022047fceb5daae0cd
zly2006 <return_2@qq.com>
2021-04-30 11:20:06 +0800

    update Items.java

--- src/main/java/org/steve/betterattack/Items.java
+++ src/main/java/org/steve/betterattack/Items.java
@@ -1,4 +1,20 @@
 package org.steve.betterattack;
 
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+
 public class Items {
+    int star;
+    String id;
+    String displayName;
+    String description;
+    Material material;
+    public Items(ConfigurationSection config){
+        id=config.getName();
+        displayName=config.getString("name");
+        material=Material.getMaterial(config.getString("material").toUpperCase());
+        star=config.getInt("star");
+        if(config.contains("description"))
+        description=config.getString("description");
+    }
 }
